831bab82af676165691589695a24adc41796db10
kdhdd <kdso10@naver.com>
2025-12-17 16:18:03 +0900

    test: CodefCredentialFixture

---
+++ src/test/java/io/codef/api/fixture/CodefCredentialFixture.java
@@ -0,0 +1,8 @@
+package io.codef.api.fixture;
+
+public class CodefCredentialFixture {
+
+	public static final String clientId = System.getenv("CLIENT_ID");
+	public static final String clientSecret = System.getenv("CLIENT_SECRET");
+	public static final String publicKey = System.getenv("PUBLIC_KEY");
+}
